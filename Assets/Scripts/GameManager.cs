45a2ba8d96ebf0c92b88eee9b4265ee406b94659
Aaron Brunckhurst <dbrunckh@mywvc.wvc.edu>
2019-11-07 17:22:10 -0800

    Added a basic MainMenu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public static void LoadGame(GameModes mode)
+    {
+        switch(mode)
+        {
+            case GameModes.Procedural3D:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Procedural");
+                break;
+            case GameModes.Story3D:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Story3D");
+                break;
+            case GameModes.Story2D:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Story2D");
+                break;
+            case GameModes.Sample:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+                break;
+        }
+    }
+
+    public enum GameModes
+    {
+        Story3D,
+        Procedural3D,
+        Story2D,
+        Sample,
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadGameProcedural3D()
+    {
+        GameManager.LoadGame(GameManager.GameModes.Procedural3D);
+    }
+    public void LoadGameStory3D()
+    {
+        GameManager.LoadGame(GameManager.GameModes.Story3D);
+    }
+    public void LoadGameStory2D()
+    {
+        GameManager.LoadGame(GameManager.GameModes.Story2D);
+    }
+    public void LoadGameSample()
+    {
+        GameManager.LoadGame(GameManager.GameModes.Sample);
+    }
+
+    public void LoadGame(GameManager.GameModes gameMode)
+    {
+        GameManager.LoadGame(gameMode);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
